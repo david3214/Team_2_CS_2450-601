43c1a72a39044f8ac431f53f3c649821c8d7c6ce
david3214 <davidw3214@gmail.com>
2022-11-02 13:25:06 -0600

    Report page (#87)
    * Added method generate_pay_report that will generate a pay report template
    
    * Basic Pay report functionality, isn't set up to use employee object yet
    
    * Added basic functionality for report page, still needs work
    
    * Report Page complete, minus button functionality and navigation bar
    
    * Added a few dummy functions

---
+++ styles.py
@@ -0,0 +1,12 @@
+## Colors
+background_color = 'grey'
+text_color = 'white'
+btn_color = '#40719A'
+
+## Fonts
+sm_text = 'Arial 10' 
+med_text = 'Arial 15'
+lg_text = 'Arial 20'
+sm_bold = 'Arial 10 bold' 
+med_bold = 'Arial 15 bold'
+lg_bold = 'Arial 20 bold'

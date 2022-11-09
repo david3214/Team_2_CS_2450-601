1e5de439df06455038a599849af75a2995fa14fb
ComputationalPotato <blkiely5@gmail.com>
2022-11-09 15:07:33 -0700

    offline

--- tests/tests.py
+++ tests/tests.py
@@ -4,5 +4,9 @@ import EmployeeContainer
 
 
 def test_name_set():
-    emp = Employee.Employee()
-    empC = EmployeeContainer.EmployeeContainer(emp)
+    emp = Employee.Employee(name="bob")
+    assert emp.name == "bob"
+    empC = EmployeeContainer.EmployeeAdmin(emp)
+    assert empC.Name == "bob"
+    empC.Name = "joe"
+    assert emp.name == "joe" == empC.Name

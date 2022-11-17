8e3672bb362727d8481f4ea0017848614092c73e
ComputationalPotato <blkiely5@gmail.com>
2022-11-17 14:28:24 -0700

    added patch to enable logging in as Karina Gay

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from GUI.Screens.Search import Search
 from config import DB
 from Database import Database
 from pathlib import Path
-
+from GUI.Components.Panels.PermittedInfo import PermittedInfo
 def main():
+    #this is debug must remove for use versions
+    DB.search(name="Karina Gay")[0].Password="688997"
     window = GUI.Window.Window()
     window.switchFrame(Login)
     window.mainloop()
-    db = Database(Path("employees.csv"))
-    db.exportDB(Path("out.csv"), adminInfo=True)
+
 
 
 if __name__ == '__main__':

59831ed5b506dddf96d6b8049d78cd954b235c35
ComputationalPotato <blkiely5@gmail.com>
2022-11-28 13:13:01 -0700

    fixed bug where updating DB would remove all fields the curr user did not have permisiion to view. added reporting errors in importing, needs to be changed to work with UI

--- Database.py
+++ Database.py
@@ -8,7 +8,7 @@ import csv
 from tkinter import messagebox
 import configparser
 import dataclasses
-from config import fetch_resource, DB_PATH
+import config
 
 empToContainer = {'active': 'Active', 'address': 'Address', 'apartment': 'Apartment', 'bank_info': 'BankInfo', 'city': 'City', 'commission': 'Commission', 'country': 'Country', 'D_O_B': 'DOB', 'Dept': 'Dept', 'Emp_ID': 'EmpID', 'End_Date': 'EndDate', 'home_email': 'HomeEmail', 'home_phone': 'HomePhone', 'hourly': 'Hourly',
                   'name': 'Name', 'Office_email': 'OfficeEmail', 'office_phone': 'OfficePhone', 'hashed_password': 'Password', 'pay_method': 'PayMethod', 'Permission_level': 'PermissionLevel', 'permitted_lock_on': 'PermittedLockOn', 'route': 'Route', 'SS_num': 'SSNum', 'salary': 'Salary', 'Start_Date': 'StartDate', 'state': 'State', 'Title': 'Title', 'zip': 'Zip'}
@@ -24,6 +24,7 @@ finally:
     with open('config.ini', 'w') as configfile:
         readcfg.write(configfile)
 # importTranslator[keyFromFile]=corresponding attribute of Employee
+#if a key is missing from config.ini then default is the name of the attribute of Employee
 importTranslator = dict([(readcfg['TRANSLATION'].get(k.name, empToContainer[k.name]), k.name)for k in dataclasses.fields(Employee)])
 empToOut = dict([(importTranslator[k], k)for k in importTranslator])
 
@@ -112,6 +113,10 @@ class Database:
                 csvReader = csv.DictReader(csvfile)
                 for row in csvReader:
                     params = dict([(importTranslator[k], row[k]) for k in row if k in importTranslator])
+                    for k in row:
+                        if k not in importTranslator:
+                            print("translation missing from config.ini for: "+str(k))
+
                     # if there are different fields from Employee class they are treated as the param **garbage
                     impList.append(Employee(**params))
             for emp in impList:
@@ -130,8 +135,8 @@ class Database:
             self.exportDB(self.initFPath, adminInfo=True,showArchivedEmployees=True)
         elif importFPath != str(INVALID_PATH):
             print("not a filepath\n")
-
-    def exportDB(self, exportPath: Path = INVALID_PATH, adminInfo: Boolean = False, showArchivedEmployees: Boolean = True) -> None:
+#save should only be True if this is called from self.save()
+    def exportDB(self, exportPath: Path = INVALID_PATH, adminInfo: Boolean = False, showArchivedEmployees: Boolean = True, save:Boolean=False) -> None:
         """_summary_
 
         Args:
@@ -144,24 +149,25 @@ class Database:
             fieldnames = dir(EmployeeContainer)
             # Switched to this because it was getting hard to keep track of all the functions on the employee container
             badnames = [method for method in fieldnames if callable(getattr(EmployeeContainer, method))] + ['permissionList']
-            badnames = badnames if adminInfo else adminFields + badnames
             fieldnames = list(filter(lambda x: x[:1] != "_" and x not in badnames, fieldnames))
 
             writer = csv.DictWriter(csvfile, fieldnames=importTranslator, restval='')
             writer.writeheader()
             for emp in self.employeeList:
-                if adminInfo:
+                if save:
+                    emp=EmployeeAdmin(emp)
+                elif adminInfo:
                     emp = self.__employeeContainment(emp, userSession)
                 else:
                     emp = EmployeeOther(emp)
 
-                if not showArchivedEmployees and not emp.Active:
+                if not save and not showArchivedEmployees and not emp.Active:
                     continue
                 out = dict([(empToOut[contToEmp[k]], emp.__getattribute__(k)) for k in fieldnames])
                 writer.writerow(out)
 
     def save(self):
-        self.exportDB(fetch_resource(DB_PATH), True,True)
+        self.exportDB(config.fetch_resource(config.DB_PATH), save=True)
 
     def __employeeContainment(self, targetEmployee: Employee, selfEmployee: EmployeeSelf):
         """takes employee and puts it in a container based on who the user is

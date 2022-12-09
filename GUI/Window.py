c6f26234c48602d5a5d363a2d5f75ad0eab1f375
ComputationalPotato <blkiely5@gmail.com>
2022-12-09 13:35:48 -0700

    replaced NULL with None, removed dependency on Windows

--- GUI/Window.py
+++ GUI/Window.py
@@ -3,7 +3,6 @@
 '''
 
 
-from asyncio.windows_events import NULL
 import tkinter as tk
 from typing import Type
 from GUI.Components.Navigation import Navigation
@@ -11,6 +10,7 @@ from GUI.Screens.Login import Login
 from Employee.EmployeeContainer import EmployeeContainer
 from Config.styles import background_color
 import typing
+
 
 class Window(tk.Tk):
     def __init__(self) -> None:
@@ -21,22 +21,22 @@ class Window(tk.Tk):
         self.resizable(False, False)
         self.frame = None
         self.configure(bg=background_color)
-        self.navigation = NULL
+        self.navigation = None
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=30)
         self.columnconfigure(0, weight=1)
 
-    def switchFrame(self, _frame: tk.Frame|Type[tk.Frame], emp: typing.Optional[EmployeeContainer]=None) -> None:
+    def switchFrame(self, _frame: tk.Frame | Type[tk.Frame], emp: typing.Optional[EmployeeContainer] = None) -> None:
         '''Destroys current frame and packs a new one'''
-        if issubclass(type(_frame) ,tk.Frame):
-            _frame=type(_frame)
+        if issubclass(type(_frame), tk.Frame):
+            _frame = type(_frame)
         if self.frame is not None:
             self.frame.destroy()
 
         if _frame is Login:
-            self.navigation = NULL
+            self.navigation = None
         else:
             self.navigation = Navigation(self)
             self.navigation.highlight_section(str(_frame))
 
-        self.frame = typing.cast('type',_frame)(self) if emp ==None else typing.cast('type',_frame)(self, emp)
+        self.frame = typing.cast('type', _frame)(self) if emp == None else typing.cast('type', _frame)(self, emp)
